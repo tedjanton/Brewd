dfc2332c08a53b403111c0dfc81b54c91c88d4ab
Lauren Chambers <laurenchambers11@icloud.com>
2021-03-21 14:31:21 -0500

    model changes

--- migrations/versions/20210321_143027_models.py
+++ migrations/versions/20210321_143027_models.py
@@ -1,8 +1,8 @@
-"""created models
+"""models
 
-Revision ID: a152b3cd7bd2
+Revision ID: a2477e1ec52d
 Revises: 
-Create Date: 2021-03-21 14:13:30.830259
+Create Date: 2021-03-21 14:30:27.856616
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'a152b3cd7bd2'
+revision = 'a2477e1ec52d'
 down_revision = None
 branch_labels = None
 depends_on = None
