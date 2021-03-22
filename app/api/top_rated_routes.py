29deae2da8ee9a1295373a1fd2ecd92fd3bfc04e
Lauren Chambers <laurenchambers11@icloud.com>
2021-03-22 14:35:53 -0500

    top rated route

--- app/api/top_rated_routes.py
+++ app/api/top_rated_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required
-from app.models import db, Sip
+from app.models import db, Coffee
 
 top_rated_routes = Blueprint('top_rated', __name__)
 
@@ -8,6 +8,6 @@ top_rated_routes = Blueprint('top_rated', __name__)
 @top_rated_routes.route('/')
 # @login_required
 def top_rated():
-    top_rated = Sip.query.order_by(Sip.rating)
+    top_rated = Coffee.query.order_by(Coffee.rating)
     print('HELLO: ', top_rated)
-    return {"top_rated": [top_rated.to_dict() for sip in top_rated]}
+    return {"top_rated": [top_rated.to_dict() for coffee in top_rated]}
