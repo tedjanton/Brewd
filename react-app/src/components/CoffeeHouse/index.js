258941fdb83a45eb7841877739ba0354888405c9
olivianicole <onyoung@umich.edu>
2021-03-23 11:47:06 -0400

    separated component

--- react-app/src/components/CoffeeHouse/index.js
+++ react-app/src/components/CoffeeHouse/index.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 import { getSips } from "../../store/coffeehouse";
 import "./CoffeeHouse.css";
+import Sip from "../Sip";
 
 const CoffeeHouse = ({ authenticated }) => {
     const dispatch = useDispatch();
@@ -28,27 +29,9 @@ const CoffeeHouse = ({ authenticated }) => {
      return (
         <div className="sips_container">
             <div className="page_title">Recent Global Activity</div>
-            <div className="sip">
-                <div className="user_input_container">
-                    <p className="text"><a className="changing_text" >Demo</a>is sipping a <a className="changing_text">Malt Ball Latte</a>at<a className="changing_text">ReAnimator Coffee</a></p>
-                    <div className="review_container">
-                        <div className="inner_container">
-                            <div className="review_text">super tasty and unique!</div>
-                            <div className="review_middle_container">
-                                <div className="review_stars"></div>
-                                <div className="type"> <i className="fas fa-mug-hot icon"></i>hot</div>
-                            </div>
-                        </div>
-                        <img className="user_image"></img>
-                        <div className="review_bottom_container">
-                            <div className="review_date">7/12/2020</div>
-                            <div className="open_sip_details">View Sip Details</div>
-                        </div>
-                    </div>
-                </div>
-                <img className="sip_logo" src="https://pbs.twimg.com/profile_images/833752955862052868/f59GA6nj_400x400.jpg"/>
-
-            </div>
+            {sips.map((sip) => {
+                <Sip />
+            })}
         </div>
 
     )
