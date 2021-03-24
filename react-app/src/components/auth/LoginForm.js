b10043883d3eb4eb2737f5c2db32587a53a9c2cc
olivianicole <onyoung@umich.edu>
2021-03-23 21:11:50 -0400

    partial styling of login form

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useDispatch } from 'react-redux';
 import { Redirect } from "react-router-dom";
 import { login } from "../../store/session";
-
+import "./LoginForm.css";
 const LoginForm = ({ authenticated, setAuthenticated }) => {
   const dispatch = useDispatch();
   const [errors, setErrors] = useState([]);
@@ -33,34 +33,56 @@ const LoginForm = ({ authenticated, setAuthenticated }) => {
   }
 
   return (
-    <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error) => (
-          <div>{error}</div>
-        ))}
+    <div className="login_form_container">
+      <div className="login_form_white_background">
+        <form  className="login_form"onSubmit={onLogin}>
+          <div>
+            {errors.map((error) => (
+              <div>{error}</div>
+            ))}
+          </div>
+
+          <div className="site_title">BREWD</div>
+          <div className="saying_container">
+            <p className="site_saying">S I P</p><p className="site_saying">S O C I A L L Y</p>
+          </div>
+
+          <button className="demo_user_login">
+            <div className="demo_text_container">
+              <p className="demo_text_small">Sign in as</p>
+              <p className="demo_text_large">demo</p>
+            </div>
+          </button>
+
+          <div className="circle_OR">OR</div>
+
+          <div className="email_container">
+            <i className="fas fa-user user_icon"></i>
+            <input
+              name="email"
+              type="text"
+              placeholder="Email"
+              value={email}
+              onChange={updateEmail}
+              className="email_input"
+            />
+          </div>
+          <div className="password_container">
+            <i class="fas fa-lock password_icon"></i>
+            <input
+              name="password"
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={updatePassword}
+              className="password_input"
+            />
+          </div>
+          <button type="submit" className="sign_in_button">Sign In</button>
+        </form>
       </div>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input
-          name="email"
-          type="text"
-          placeholder="Email"
-          value={email}
-          onChange={updateEmail}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input
-          name="password"
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={updatePassword}
-        />
-        <button type="submit">Login</button>
-      </div>
-    </form>
+    </div>
+      
   );
 };
 
