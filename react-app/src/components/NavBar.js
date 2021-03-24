218c3115dbf2f5e586060557d2f65d1b6a41e98c
Ted Anton <tedjanton@gmail.com>
2021-03-23 21:51:47 -0500

    added hover effects to buttons on landing

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -19,7 +19,7 @@ const NavBar = ({ authenticated, setAuthenticated }) => {
             </NavLink>
           </li>
           <li>
-            <NavLink to="/sign-up" exact={true} activeClassName="active">
+            <NavLink to="/signup" exact={true} activeClassName="active">
               Sign Up
             </NavLink>
           </li>
