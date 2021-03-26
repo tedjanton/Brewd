c8dcef7d1876d413f8e846d165a5db9dbbbc2a5f
olivianicole <onyoung@umich.edu>
2021-03-26 10:12:10 -0400

    route fully working and html done

--- react-app/src/components/Shop/index.js
+++ react-app/src/components/Shop/index.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getShop } from "../../store/coffeeshop_details";
+import IndividualShop from "../IndividualShop";
 import "./Shop.css";
 
 const ShopDetails = () => {
@@ -9,8 +10,9 @@ const ShopDetails = () => {
     const dispatch = useDispatch();
 
     const shop = useSelector((state) => {
-        console.log(state.shop)
+        return state.shop?.shop?.individual_shop
     });
+    console.log("SHOP", shop)
 
     useEffect(() => {
         if (!shop) {
@@ -20,6 +22,16 @@ const ShopDetails = () => {
 
    return (
        <>
+            <div className="shop_details_page_container">
+                <div className="shop_details_page_container_top">
+                    <div className="individual_shop_container">
+                        <IndividualShop shop={shop} />
+                    </div>
+                </div>
+                <div className="shop_details_page_container_bottom">
+
+                </div>
+            </div>
        </>
    )
 }
