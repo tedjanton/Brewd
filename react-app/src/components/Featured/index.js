d021d38209742a67955d90874b44dbc305564f59
Ted Anton <tedjanton@gmail.com>
2021-03-26 16:16:36 -0500

    made featured coffees clickable

--- react-app/src/components/Featured/index.js
+++ react-app/src/components/Featured/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux"
+import { Link } from "react-router-dom";
 import { getFeaturedCoffees } from "../../store/featured"
 import "./Featured.css"
 
@@ -20,7 +21,7 @@ const Featured = () => {
                 <div key={coffee.id} className="featured_coffee">
                     <img className="featured_coffee_img" src={coffee.img_src}/>
                     <div className="featured_text_container">
-                        <p className="featured_coffee_name">{coffee.name}</p>
+                        <Link to={`/coffees/${coffee.id}`} className="featured_coffee_name">{coffee.name}</Link>
                         <p className="featured_coffee_shop">{coffee.shop.name}</p>
                     </div>
                 </div>
